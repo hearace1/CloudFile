84c4bf6048224342a058f042fcb063c4dad154cf
hearace1 <hearace@hotmail.com>
2016-09-07 19:44:51 +0800

    add thumbnail

--- src/com/hearace/cloudfile/model/UploadItemListAdapter.java
+++ src/com/hearace/cloudfile/model/UploadItemListAdapter.java
@@ -1,24 +1,36 @@
 package com.hearace.cloudfile.model;
 
+import java.io.File;
+
 import com.hearace.cloudfile.R;
 
+import android.content.ContentResolver;
 import android.content.Context;
 import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.provider.MediaStore.Images.Thumbnails;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
 public class UploadItemListAdapter extends BaseAdapter {
 
+	private Context context = null;
 	private LayoutInflater mInflater = null;
 	private Cursor cur = null;
-	
+	private ContentResolver cr = null;
 	public UploadItemListAdapter(Context context, Cursor cur) {
+		this.context = context;
 		this.mInflater = LayoutInflater.from(context);
 		this.cur = cur;
+		cr = context.getContentResolver();  
 	}
 
 	@Override
@@ -57,6 +69,7 @@ public class UploadItemListAdapter extends BaseAdapter {
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
 		// TODO Auto-generated method stub
+		cur.moveToPosition(position);
 		ViewHolderItem viewHolder;
 		if(convertView == null){
 			viewHolder = new ViewHolderItem();
@@ -64,20 +77,24 @@ public class UploadItemListAdapter extends BaseAdapter {
 			viewHolder.fileNameTextView = (TextView)convertView.findViewById(R.id.uploadFileNameTV);
 			viewHolder.uploadProgress = (ProgressBar)convertView.findViewById(R.id.uploadProgress);
 			viewHolder.uploadProgress.setMax(100);
-			viewHolder.uploadStatusTextView = (TextView)convertView.findViewById(R.id.uploadStatus);
+//			viewHolder.uploadStatusTextView = (TextView)convertView.findViewById(R.id.uploadStatus);
 			viewHolder.errMsgTextView = (TextView)convertView.findViewById(R.id.errMsgTV);
 			viewHolder.fileId = (TextView)convertView.findViewById(R.id.fileId);
+			viewHolder.thumbnail = (ImageView) convertView.findViewById(R.id.imageView1);
 			convertView.setTag(viewHolder);
 		}else{
 			viewHolder = (ViewHolderItem) convertView.getTag();
 		}
 		
-		cur.moveToPosition(position);
-		int status = cur.getInt(cur.getColumnIndex("status"));
 		int fileId = cur.getInt(cur.getColumnIndex("_id"));
 		String fileName = cur.getString(cur.getColumnIndex("file"));
 		viewHolder.fileNameTextView.setText(fileName);
 		viewHolder.fileId.setText(String.valueOf(fileId));
+		Bitmap thumbnail = getThumbnail(fileName);
+		if(thumbnail != null)
+			viewHolder.thumbnail.setImageBitmap(thumbnail);
+
+		int status = cur.getInt(cur.getColumnIndex("status"));
 		int progressPercent = 0;
 		long progress = cur.getLong(cur.getColumnIndex("progress"));
 		long fileSZ = cur.getLong(cur.getColumnIndex("fileSZ"));
@@ -85,23 +102,26 @@ public class UploadItemListAdapter extends BaseAdapter {
 		case 100:
 			progressPercent = 100;
 			viewHolder.errMsgTextView.setVisibility(TextView.GONE);
-			viewHolder.uploadStatusTextView.setText("Completed");
+//			viewHolder.uploadStatusTextView.setText("Completed");
+			viewHolder.uploadProgress.getProgressDrawable().setColorFilter(Color.GREEN, android.graphics.PorterDuff.Mode.SRC_IN);
 			break;
 		case -1:
 			progressPercent = (int) (progress * 100 / fileSZ);
-			viewHolder.uploadStatusTextView.setText("Failed");
+//			viewHolder.uploadStatusTextView.setText("Failed");
 			String errMsg = cur.getString(cur.getColumnIndex("errMsg"));
 			viewHolder.errMsgTextView.setText(errMsg);
 			viewHolder.errMsgTextView.setVisibility(TextView.VISIBLE);
+			viewHolder.uploadProgress.getProgressDrawable().setColorFilter(Color.RED, android.graphics.PorterDuff.Mode.SRC_IN);
 			break;
 		case 1:
 			progressPercent = (int) (progress * 100 / fileSZ);
 			viewHolder.errMsgTextView.setVisibility(TextView.GONE);
-			viewHolder.uploadStatusTextView.setText("Uploading");
+//			viewHolder.uploadStatusTextView.setText("Uploading");
+			viewHolder.uploadProgress.getProgressDrawable().setColorFilter(Color.YELLOW, android.graphics.PorterDuff.Mode.SRC_IN);
 			break;
 		case 0:
 			progressPercent = 0;
-			viewHolder.uploadStatusTextView.setText("Wait");
+//			viewHolder.uploadStatusTextView.setText("Wait");
 			viewHolder.errMsgTextView.setVisibility(TextView.GONE);
 			break;
 		}
@@ -109,12 +129,25 @@ public class UploadItemListAdapter extends BaseAdapter {
 		return convertView;
 	}
 	
+	private Bitmap getThumbnail(String filePath){
+		Cursor cur1 = cr.query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, 
+				new String[]{
+						MediaStore.Images.Media._ID
+		}, MediaStore.Images.Media.DATA + "=?", new String[]{filePath}, null);
+		if(cur1.moveToFirst()){
+			Bitmap bm = Thumbnails.getThumbnail(cr, cur1.getInt(0), Thumbnails.MICRO_KIND, null);
+			return bm;
+		}
+		return null;
+	}
+	
 	static class ViewHolderItem{
 		TextView fileNameTextView;
 		ProgressBar uploadProgress;
-		TextView uploadStatusTextView;
+//		TextView uploadStatusTextView;
 		TextView errMsgTextView;
 		TextView fileId;
+		ImageView thumbnail;
 	}
 }
 
